66f6c85869dba25b399bd8045c94d13253851a0d
Mark Valdez <mhvaldez209@gmail.com>
2021-01-21 06:01:15 -1000

    setting up express

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
