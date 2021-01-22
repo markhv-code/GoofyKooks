7cc7e465efeaf9f0b47c37c4bec4ff0672134c30
Mark Valdez <mhvaldez209@gmail.com>
2021-01-21 22:05:41 -1000

    homework done/wrap-up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,6 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 
-
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
-
 router.use('/api', apiRouter);
 
 module.exports = router;
