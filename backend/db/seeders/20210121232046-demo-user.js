cd48e71e6ab43d7eedbc60938ea60ba92567a179
Mark Valdez <mhvaldez209@gmail.com>
2021-01-21 21:25:39 -1000

    finished testing backend

---
+++ backend/db/seeders/20210121232046-demo-user.js
@@ -0,0 +1,38 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+const user = require('../models/user');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: "mark@noreply.io",
+        username: 'Goku',
+        hashedPassword: bcrypt.hashSync('aB1'),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
