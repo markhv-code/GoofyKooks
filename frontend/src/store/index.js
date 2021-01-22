340490da53cbf789bdd98308a768c48fa8659618
Mark Valdez <mhvaldez209@gmail.com>
2021-01-22 07:52:39 -1000

    front end setup connect to backend

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+
 const rootReducer = combineReducers({
   // add reducer functions here
 });
@@ -19,5 +20,7 @@ if (process.env.NODE_ENV === "production") {
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
+
 
 export default configureStore;
