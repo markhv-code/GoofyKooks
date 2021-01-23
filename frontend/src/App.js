4cca3b918396daf25d90e5f3a26c90a63555eea3
Mark Valdez <mhvaldez209@gmail.com>
2021-01-23 09:31:28 -1000

    login route now a redux portal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-
 
 function App() {
   const dispatch = useDispatch();
@@ -19,17 +17,13 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
         </Switch>
-  )}
-  </>
+      )}
+    </>
   );
 }
-
 
 export default App;
