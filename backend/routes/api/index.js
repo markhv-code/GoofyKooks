1af6a9066f0e7ece8548e0943e556f0046eeb667
Mark Valdez <mhvaldez209@gmail.com>
2021-01-25 09:19:13 -1000

    add createSBpage backend route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const surfBreaksRouter = require('./surf_breaks.js');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/surf_breaks', surfBreaksRouter);
 
 module.exports = router;
