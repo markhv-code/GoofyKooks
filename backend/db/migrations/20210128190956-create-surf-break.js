0970d71ad41aa7d0ff6ac55b9f8f32c173b3a3d9
Mark Valdez <mhvaldez209@gmail.com>
2021-01-28 11:45:54 -1000

    generate surfBreak migration/model

---
+++ backend/db/migrations/20210128190956-create-surf-break.js
@@ -0,0 +1,63 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('surfBreaks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+      country: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      latitude: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      longitude: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      zipcode: {
+        type: Sequelize.FLOAT(5),
+        allowNull: false,
+      },
+      waveType: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('surfBreaks');
+  }
+};
---
+++ backend/db/models/surfbreak.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const surfBreak = sequelize.define('surfBreak', {
+    name: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
+    country: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    private: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    latitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    zipcode: {
+      type: DataTypes.FLOAT(5),
+      allowNull: false,
+    },
+    waveType: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    }
+  }, {});
+  surfBreak.associate = function(models) {
+    SurfBreak.hasMany(models.Review, { foreignKey: 'breakId' });
+  };
+  return surfBreak;
+};
