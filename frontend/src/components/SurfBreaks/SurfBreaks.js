de19c006cf79cf894614522129c821ee67290618
Mark Valdez <mhvaldez209@gmail.com>
2021-01-29 11:57:26 -1000

    add new surfBreak component

---
+++ frontend/src/components/SurfBreaks/SurfBreaks.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import * as surfActions from "../../store/surfBreak"
+
+function SurfBreaks() {
+    const dispatch = useDispatch();
+    const surfBreaks = useSelector((state) => Object.values(state.surfBreak));
+
+    useEffect(() => {
+        dispatch(surfActions.getSurfBreaks())
+    }, [dispatch])
+
+    return (
+        <>
+        <h1>Hey there</h1>
+        {
+            surfBreaks.map(surfBreak => (
+                <h1>{surfBreak.name}
+                    <ul>
+                        <li key={`{surfBreak.country}`}>{surfBreak.country}</li>
+                        <li key={`{surfBreak.state}`}>{surfBreak.state}</li>
+                        <li key={`{surfBreak.city}`}>{surfBreak.city}</li>
+                        <li key={`{surfBreak.privatebreak}`}>{surfBreak.privatebreak}</li>
+                        <li key={`{surfBreak.latitude}`}>{surfBreak.latitude}</li>
+                        <li key={`{surfBreak.longitude}`}>{surfBreak.longitude}</li>
+                        <li key={`{surfBreak.zipcode}`}>{surfBreak.zipcode}</li>
+                        <li key={`{surfBreak.waveType}`}>{surfBreak.waveType}</li>
+                    </ul>
+                </h1>
+            ))
+        }
+        </>
+    )
+}
+
+export default SurfBreaks;
